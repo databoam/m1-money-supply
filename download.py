95957ac335d4d01f0836df9c345a1c9e585bb2fd
databoam <165313095+databoam@users.noreply.github.com>
2025-06-21 16:27:34 +0000

    download m1 data

---
+++ download.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import requests   
+from datetime import datetime
+url_endpoint = 'http://www.bankofengland.co.uk/boeapps/iadb/fromshowcolumns.asp?csv.x=yes'
+
+today = datetime.now().strftime('%d/%b/%Y')
+print(today)
+
+payload = {
+    'Datefrom'   : '01/Jan/2010',
+    'Dateto'     :  today,
+    'SeriesCodes': 'LPMZ3TH,LPMB6S2,LPMB6S3,LPMB75C,LPMB76C,LPMVYWO,LPMZ3TT,LPMZ3TV,LPMZ3TX,LPMVWLQ,LPMVWLW,RPMB3OM',
+    'CSVF'       : 'TN',
+    'UsingCodes' : 'Y',
+    'VPD'        : 'Y',
+    'VFD'        : 'N'
+}
+
+headers = {
+    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) '
+                 'AppleWebKit/537.36 (KHTML, like Gecko) '
+                 'Chrome/54.0.2840.90 '
+                 'Safari/537.36'
+}
+
+response = requests.get(url_endpoint, params=payload, headers=headers)
+
+
+# Check if the response was successful, it should return '200'
+print(response.status_code)
+
+with open('m1_comp_raw.csv', 'w', encoding='utf-8') as f:
+    f.write(response.text)
+
+
--- process.py
+++
@@ -1,9 +0,0 @@
-#Process M1 money supply data
-import pandas as pd
-
-src = '''https://www.bankofengland.co.uk/boeapps/database/fromshowcolumns.asp?Travel
-=NIxAZxSUx&FromSeries=1&ToSeries=50&DAT=RNG&FD=1&FM=Jan&FY
-=2010&TD=31&TM=Dec&TY=2040&FNY=Y&CSVF=TT&html.x=66&html.y
-=26&SeriesCodes=LPMZ3TH&UsingCodes=Y&Filter=N&title=LPMZ3TH&VPD=Y#'''
-
-# do stuff
